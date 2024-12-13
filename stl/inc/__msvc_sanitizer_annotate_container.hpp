25b8a6895ce22a97af4b5af493c0e82723669dc1
Casey Carter <cacarter@microsoft.com>
2024-12-12 22:42:52 -0800

    Add deque annotation support

--- stl/inc/__msvc_sanitizer_annotate_container.hpp
+++ stl/inc/__msvc_sanitizer_annotate_container.hpp
@@ -18,11 +18,13 @@ _STL_DISABLE_CLANG_WARNINGS
 // The following macros change the behavior of this file:
 //   - _DISABLE_STL_ANNOTATION: Disable ASan annotations in the standard library
 //     (this will be auto-defined on unsupported platforms)
+//     + _DISABLE_DEQUE_ANNOTATION: same, but for only `deque`
 //     + _DISABLE_STRING_ANNOTATION: same, but for only `basic_string`
 //     + _DISABLE_VECTOR_ANNOTATION: same, but for only `vector`
 //   - _ENABLE_STL_ANNOTATION_ON_UNSUPPORTED_PLATFORMS: Don't auto-disable ASan annotations
 //   - _ANNOTATE_STL: Even when ASan annotations are disabled, insert the code for annotating into the STL anyways;
 //     this is useful when building static libraries which may be linked against both ASan and non-ASan binaries.
+//     + _ANNOTATE_DEQUE: same, but only for `deque`
 //     + _ANNOTATE_STRING: same, but only for `basic_string`
 //     + _ANNOTATE_VECTOR: same, but only for `vector`
 
@@ -40,6 +42,9 @@ _STL_DISABLE_CLANG_WARNINGS
 #error _ENABLE_STL_ANNOTATION_ON_UNSUPPORTED_PLATFORMS and _DISABLE_STL_ANNOTATION are mutually exclusive
 #endif // ^^^ defined(_ENABLE_STL_ANNOTATION_ON_UNSUPPORTED_PLATFORMS) ^^^
 
+#ifndef _DISABLE_DEQUE_ANNOTATION
+#define _DISABLE_DEQUE_ANNOTATION
+#endif // ^^^ !defined(_DISABLE_DEQUE_ANNOTATION) ^^^
 #ifndef _DISABLE_STRING_ANNOTATION
 #define _DISABLE_STRING_ANNOTATION
 #endif // ^^^ !defined(_DISABLE_STRING_ANNOTATION) ^^^
@@ -50,6 +55,10 @@ _STL_DISABLE_CLANG_WARNINGS
 #endif // ^^^ defined(_DISABLE_STL_ANNOTATION) ^^^
 
 #ifdef _ANNOTATE_STL
+
+#ifndef _ANNOTATE_DEQUE
+#define _ANNOTATE_DEQUE
+#endif // ^^^ !defined(_ANNOTATE_DEQUE) ^^^
 
 #ifndef _ANNOTATE_STRING
 #define _ANNOTATE_STRING
@@ -63,6 +72,8 @@ _STL_DISABLE_CLANG_WARNINGS
 
 #ifdef __SANITIZE_ADDRESS__
 
+#define _ACTIVATE_DEQUE_ANNOTATION
+#define _INSERT_DEQUE_ANNOTATION
 #define _ACTIVATE_STRING_ANNOTATION
 #define _INSERT_STRING_ANNOTATION
 #define _ACTIVATE_VECTOR_ANNOTATION
@@ -71,6 +82,8 @@ _STL_DISABLE_CLANG_WARNINGS
 #elif defined(__clang__) // ^^^ defined(__SANITIZE_ADDRESS__) / defined(__clang__) vvv
 
 #if __has_feature(address_sanitizer)
+#define _ACTIVATE_DEQUE_ANNOTATION
+#define _INSERT_DEQUE_ANNOTATION
 #define _ACTIVATE_STRING_ANNOTATION
 #define _INSERT_STRING_ANNOTATION
 #define _ACTIVATE_VECTOR_ANNOTATION
@@ -81,6 +94,10 @@ _STL_DISABLE_CLANG_WARNINGS
 #endif // ^^^ defined(__clang__) ^^^
 
 
+#ifdef _DISABLE_DEQUE_ANNOTATION
+#undef _ACTIVATE_DEQUE_ANNOTATION
+#undef _INSERT_DEQUE_ANNOTATION
+#endif // ^^^ defined(_DISABLE_DEQUE_ANNOTATION) ^^^
 #ifdef _DISABLE_STRING_ANNOTATION
 #undef _ACTIVATE_STRING_ANNOTATION
 #undef _INSERT_STRING_ANNOTATION
@@ -90,6 +107,9 @@ _STL_DISABLE_CLANG_WARNINGS
 #undef _INSERT_VECTOR_ANNOTATION
 #endif // ^^^ defined(_DISABLE_VECTOR_ANNOTATION) ^^^
 
+#ifdef _ANNOTATE_DEQUE
+#define _INSERT_DEQUE_ANNOTATION
+#endif // ^^^ defined(_ANNOTATE_DEQUE) ^^^
 #ifdef _ANNOTATE_STRING
 #define _INSERT_STRING_ANNOTATION
 #endif // ^^^ defined(_ANNOTATE_STRING) ^^^
@@ -98,6 +118,9 @@ _STL_DISABLE_CLANG_WARNINGS
 #endif // ^^^ defined(_ANNOTATE_VECTOR) ^^^
 
 
+#ifndef _INSERT_DEQUE_ANNOTATION
+#pragma detect_mismatch("annotate_deque", "0")
+#endif // ^^^ !defined(_INSERT_DEQUE_ANNOTATION) ^^^
 #ifndef _INSERT_STRING_ANNOTATION
 #pragma detect_mismatch("annotate_string", "0")
 #endif // ^^^ !defined(_INSERT_STRING_ANNOTATION) ^^^
@@ -105,6 +128,10 @@ _STL_DISABLE_CLANG_WARNINGS
 #pragma detect_mismatch("annotate_vector", "0")
 #endif // ^^^ !defined(_INSERT_VECTOR_ANNOTATION) ^^^
 
+#ifdef _ACTIVATE_DEQUE_ANNOTATION
+#pragma comment(lib, "stl_asan")
+#pragma detect_mismatch("annotate_deque", "1")
+#endif // ^^^ defined(_ACTIVATE_DEQUE_ANNOTATION) ^^^
 #ifdef _ACTIVATE_STRING_ANNOTATION
 #pragma comment(lib, "stl_asan")
 #pragma detect_mismatch("annotate_string", "1")
@@ -114,10 +141,13 @@ _STL_DISABLE_CLANG_WARNINGS
 #pragma detect_mismatch("annotate_vector", "1")
 #endif // ^^^ defined(_ACTIVATE_VECTOR_ANNOTATION) ^^^
 
+#undef _ACTIVATE_DEQUE_ANNOTATION
 #undef _ACTIVATE_STRING_ANNOTATION
 #undef _ACTIVATE_VECTOR_ANNOTATION
 
 extern "C" {
+#ifdef _INSERT_DEQUE_ANNOTATION
+extern const bool _Asan_deque_should_annotate;
 #endif
 
 #ifdef _INSERT_STRING_ANNOTATION
@@ -129,7 +159,7 @@ extern const bool _Asan_vector_should_annotate;
 #endif
 } // extern "C"
 
-#if defined(_INSERT_STRING_ANNOTATION) || defined(_INSERT_VECTOR_ANNOTATION)
+#if defined(_INSERT_DEQUE_ANNOTATION) || defined(_INSERT_STRING_ANNOTATION) || defined(_INSERT_VECTOR_ANNOTATION)
 #ifdef _M_ARM64EC
 #define _STL_NATIVE_SYMBOL_PREFIX "#"
 #define _STL_HYBRID_SYMBOL_PREFIX ""
@@ -156,13 +186,20 @@ extern const bool _Asan_vector_should_annotate;
 #define _STL_CODE_ALIAS(alias, target) _STL_DATA_ALIAS(alias, target)
 #endif
 
-extern "C" { // This must match ASan's primary declaration, which isn't marked `noexcept`.
+extern "C" { // These must match ASan's primary declarations, which aren't marked `noexcept`.
 void __cdecl __sanitizer_annotate_contiguous_container(
     const void* _First, const void* _End, const void* _Old_last, const void* _New_last);
+void __cdecl __sanitizer_annotate_double_ended_contiguous_container( //
+    const void* _Storage_beg, const void* _Storage_end, // Raw storage range
+    const void* _Old_first, const void* _Old_last, // Pre-call accessible range
+    const void* _New_first, const void* _New_last); // Post-call accessible range
 } // extern "C"
 
 _STL_CODE_ALIAS(__sanitizer_annotate_contiguous_container, __sanitizer_annotate_contiguous_container_default)
+_STL_CODE_ALIAS(__sanitizer_annotate_double_ended_contiguous_container,
+    __sanitizer_annotate_double_ended_contiguous_container_default)
 
+_STL_DATA_ALIAS(_Asan_deque_should_annotate, _Asan_deque_should_annotate_default)
 _STL_DATA_ALIAS(_Asan_string_should_annotate, _Asan_string_should_annotate_default)
 _STL_DATA_ALIAS(_Asan_vector_should_annotate, _Asan_vector_should_annotate_default)
 
--- stl/src/asan_noop.cpp
+++ stl/src/asan_noop.cpp
@@ -2,9 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 extern "C" {
+extern const bool _Asan_deque_should_annotate_default  = false;
 extern const bool _Asan_string_should_annotate_default = false;
 extern const bool _Asan_vector_should_annotate_default = false;
 
 void __cdecl __sanitizer_annotate_contiguous_container_default(
     const void*, const void*, const void*, const void*) noexcept {}
+void __cdecl __sanitizer_annotate_double_ended_contiguous_container_default(
+    const void*, const void*, const void*, const void*, const void*, const void*) noexcept {}
 } // extern "C"
